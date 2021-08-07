a1975e132367f3600b0768d7a038a3e3fe33a38b
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-06 20:36:56 -0700

    Update __main__.py

--- EzilaXBotV/__main__.py
+++ EzilaXBotV/__main__.py
@@ -34,7 +34,7 @@ from telegram.ext import (
 from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
 
-from AnkiVector import (
+from EzilaXBotV import (
     ALLOW_EXCL,
     BL_CHATS,
     CERT_PATH,
