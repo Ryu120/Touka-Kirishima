d0c39d1d3e346686fce609334c909ad1bf741f4b
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-27 07:23:53 -0700

    Update books.py

--- EzilaXBotV/modules/books.py
+++ EzilaXBotV/modules/books.py
@@ -25,7 +25,7 @@ import requests
 from bs4 import BeautifulSoup
 from telethon import events
 
-from EzilaXBotV.services.telethon import tbot
+from EzilaXBotV.telethon import tbot
 
 
 @tbot.on(events.NewMessage(pattern="^/book (.*)"))
