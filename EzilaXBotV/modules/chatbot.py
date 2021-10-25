5abe31e5a64923a47c67d58e0af5e3c497cf100a
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-10-25 16:46:27 +0530

    Update chatbot.py

--- EzilaXBotV/modules/chatbot.py
+++ EzilaXBotV/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Yuzuki Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
+                "ᴛᴏᴜᴋᴀ Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Yuzuki Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
+                "ᴛᴏᴜᴋᴀ Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
