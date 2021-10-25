8e005fa85b36c792d001987129a51df7a1f902c4
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-10-25 11:11:46 +0530

    Update __main__.py

--- EzilaXBotV/__main__.py
+++ EzilaXBotV/__main__.py
@@ -810,7 +810,8 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@Toukakirishimasupport", "[I am now alive!](https://telegra.ph/file/6881d75ef5d447f70aa8d.mp4)") 
+            dispatcher.bot.sendMessage(f"@Toukakirishimasupport", "[I am now alive!] 
+(https://telegra.ph/file/6881d75ef5d447f70aa8d.mp4)", parse_mode=ParseMode.MARKDOWN)        
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
